ac3bdee22ccae5323a79292d7bc6ae0c133110d5
Ayushman-2k24 <ayushmansenapati7@gmail.com>
2025-09-10 23:38:53 +0530

    pattern practise

---
+++ CPP/patterns/diamond.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int count=5;
+
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < count-i; j++)
+        {
+            cout<<" ";
+        }
+    
+        for (int j = 0; j <i+1; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j<i+1; j++)
+        {
+            cout<<" ";
+        }
+        for (int j = 0; j<count-i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+   
+}
---
+++ CPP/patterns/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+for (int i = 0; i < count; i++)
+    // {
+    //     for (int j = 0; j < count-i; i++)
+    //     {
+    //         cout<<" ";
+    //     }
+    
+    //     for (int j = 0; j <i+1; i++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
