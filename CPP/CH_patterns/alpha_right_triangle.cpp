7d7d59bb8ac10a1cb253166ee3f2577a9bfe2fc9
Ayushman-2k24 <ayushmansenapati7@gmail.com>
2025-09-17 00:28:13 +0530

    some more patterna

--- CPP/CH_patterns/alpha_right_triangle.cpp
+++ CPP/CH_patterns/alpha_right_triangle.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 int main(){
     int n=5;
+    char ch;
     for(int i=0;i<n;i++){
         for(int j=0;j<i+1;j++){
-            char ch='A'+j;
+            ch='A'+j;
             cout<<ch;
         }
-        
+        char k=ch;
+        while(k>'A'){
+            cout<<char(k-1);
+            k--;
+        }
         cout<<endl;
     }
 }
