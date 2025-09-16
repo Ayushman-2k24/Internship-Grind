3f99e8fb009485566c5d32dc76bb2d72ea714ac6
Ayushman-2k24 <ayushmansenapati7@gmail.com>
2025-09-16 22:50:49 +0530

    some more

--- CPP/patterns/no_then_star.cpp
+++ CPP/patterns/no_then_star.cpp
@@ -3,14 +3,37 @@ using namespace std;
 int main(){
     int n=4;
     for(int i=0;i<n;i++){
-        for (int  j = 0; j < i+1; j++)
-        {
-            if(j==i){
+        for(int j=0;j<2*i+1;j++){
+            if(j%2==0){
                 cout<<i+1;
             }else{
-                cout<<i+1<<"*";
+                cout<<"*";
             }
         }
         cout<<endl;
     }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n+1-2*i;j++){
+            if(j%2==0){
+                cout<<n-(i+1);
+            }else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+
+
+    // int n=4;
+    // for(int i=0;i<n;i++){
+    //     for (int  j = 0; j < i+1; j++)
+    //     {
+    //         if(j==i){
+    //             cout<<i+1;
+    //         }else{
+    //             cout<<i+1<<"*";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
 }
